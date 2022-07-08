0f8d2792b8a1130ac47329dd228af3c2dadef1c6
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-08 11:58:08 +0530

    student

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -12,5 +12,12 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  name="Nisha"
+  data=[
+    {"name":"Riya","age":20,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+    {"name":"Nisha","age":30,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+    {"name":"Fahim","age":50,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+    {"name":"Ashwin","age":25,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+    {"name":"Jinto","age":23,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+    {"name":"Brino","age":24,"photo":"https://cdn-icons-png.flaticon.com/512/2784/2784403.png"},
+   ]
 }
